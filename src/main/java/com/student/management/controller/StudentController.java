ee917ab5989bca2ff8f4f12a138da7ba98b4f1d6
Nithishkumaar-TC <nithishk1083@gmail.com>
2026-06-24 18:17:27 +0530

    demo2

--- src/main/java/com/student/management/controller/StudentController.java
+++ src/main/java/com/student/management/controller/StudentController.java
@@ -109,4 +109,12 @@ public class StudentController {
 		return ResponseEntity.ok(response);
 	}
 
+	@GetMapping("/test")
+	public String testApi() {
+	    return "Debug Branch Working";
+	}
+	@GetMapping("/demo2")
+	public String demo2() {
+		return "hello";
+	}
 }
